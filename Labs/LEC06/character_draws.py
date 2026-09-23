d36730a57f593edd12ac227eab172f4676af2482
wooseongbae <baeseongwoo0320@gmail.com>
2026-09-24 04:50:08 +0900

    전체 반복 흐름 검증

--- Labs/LEC06/character_draws.py
+++ Labs/LEC06/character_draws.py
@@ -1,17 +1,19 @@
 # 실습 과제 진행
 
 def move_circle():
+    print("원")
     pass
 
 def move_rectangle():
+    print("사각형")
     pass
 
 def move_triangle():
+    print("삼각형")
     pass
 
 while True:
     move_circle()
     move_rectangle()
     move_triangle()
-    print("원-사각형-삼각형 함수 반복 호출 완료")
     pass
