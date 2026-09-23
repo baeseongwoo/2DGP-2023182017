0964f70a191db68583230efb5e8b02dd4e01d49c
wooseongbae <baeseongwoo0320@gmail.com>
2026-09-24 04:58:29 +0900

    캔버스 생성

--- Labs/LEC06/character_draws.py
+++ Labs/LEC06/character_draws.py
@@ -1,4 +1,7 @@
 # 실습 과제 진행
+from pico2d import*
+
+open_canvas(800,600)
 
 def move_circle():
     print("원")
@@ -17,3 +20,5 @@ while True:
     move_rectangle()
     move_triangle()
     pass
+
+close_canvas()
