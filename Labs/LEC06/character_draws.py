723e760396ec5e354180b7fe6f482f599f2ee8f7
wooseongbae <baeseongwoo0320@gmail.com>
2026-09-25 14:53:17 +0900

    캐릭터 드로우 함수 흐름 확인

--- Labs/LEC06/character_draws.py
+++ Labs/LEC06/character_draws.py
@@ -8,21 +8,34 @@ clear_canvas()
 character.draw(400,300)
 update_canvas()
 
+x=400
+y=300
+
+
 def move_circle():
     print("원")
+    draw_character()
     pass
 
 def move_rectangle():
     print("사각형")
+    draw_character()
+
     pass
 
 def move_triangle():
     print("삼각형")
+    draw_character()
     pass
+
+def draw_character():
+    print("캐릭터 그리기")
 
 while True:
     move_circle()
+
     move_rectangle()
+
     move_triangle()
     pass
 
