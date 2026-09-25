9887b9e5f96fcb638cb29adbfbb0227298df5326
wooseongbae <baeseongwoo0320@gmail.com>
2026-09-26 02:52:51 +0900

    삼각형 오른쪽 구간 이동 방향 변경

--- Labs/LEC06/character_draws.py
+++ Labs/LEC06/character_draws.py
@@ -62,8 +62,8 @@ def move_triangle_left():
 
 def move_triangle_right():
     for t in range(100):
-        x=400+(600-400)*t/100
-        y=400+(200-400)*t/100
+        x=600+(400-600)*t/100
+        y=200+(400-200)*t/100
         draw_character(x,y)
 
 def move_triangle_bottom():
