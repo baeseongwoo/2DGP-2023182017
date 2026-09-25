24398f0b1b38a810d188c01368b266c2bb37593b
wooseongbae <baeseongwoo0320@gmail.com>
2026-09-26 02:45:52 +0900

    삼각형 오른쪽 구간 확인

--- Labs/LEC06/character_draws.py
+++ Labs/LEC06/character_draws.py
@@ -27,6 +27,7 @@ def move_rectangle():
 def move_triangle():
     print("삼각형")
     move_triangle_left()
+    move_triangle_right()
 
     pass
 
@@ -58,6 +59,13 @@ def move_triangle_left():
         x=200+(400-200)*t/100
         y=200+(400-200)*t/100
         draw_character(x,y)
+
+def move_triangle_right():
+    for t in range(100):
+        x=400+(600-400)*t/100
+        y=400+(200-400)*t/100
+        draw_character(x,y)
+
 
 while True:
     move_circle()
